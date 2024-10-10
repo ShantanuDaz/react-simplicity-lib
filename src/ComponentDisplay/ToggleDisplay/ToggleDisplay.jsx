0b2a33979145ebcfc4bce3ab7f89fc488972461b
ShantanuDaz <shaunofshan@gmail.com>
2024-10-10 15:53:43 +0530

    toggle display done

--- src/ComponentDisplay/ToggleDisplay/ToggleDisplay.jsx
+++ src/ComponentDisplay/ToggleDisplay/ToggleDisplay.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Toggle from "../../Components/Toggle/Toggle";
 import "./ToggleDisplay.css";
+
 const ToggleDisplay = () => {
   const [toggleStates, setToggleStates] = useState([
     { state: false, ballColorOn: "pink" },
@@ -8,17 +9,33 @@ const ToggleDisplay = () => {
     { state: false, ballColorOn: "violet" },
     { state: false, ballColorOn: "yellow" },
   ]);
+
   useEffect(() => {
-    toggleStates.forEach((toggle, i) => {
-      setInterval(() => {
-        setToggleStates((prev) => {
-          const newToggles = [...prev];
-          newToggles[i].state = !newToggles[i].state;
-          return newToggles;
-        });
-      }, 2000 + i * 100);
-    });
-  }, []);
+    for (let i = 0; i <= 3; i++) {
+      if (toggleStates[i].state === false) {
+        setTimeout(() => {
+          if (i === 3) {
+            setToggleStates((prev) => {
+              let temp = [...prev];
+              temp[i].state = true;
+              temp[0].state = false;
+              return temp;
+            });
+          } else {
+            setToggleStates((prev) => {
+              let temp = [...prev];
+              temp[i].state = true;
+              temp[i + 1].state = false;
+              return temp;
+            });
+          }
+        }, 1000);
+
+        break;
+      }
+    }
+  }, [toggleStates]);
+
   return (
     <section id="toggle-displayName">
       <h2>Toggle</h2>
