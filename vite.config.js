226d460af481b4b84eb9093f7afbc56719ad0c21
ShantanuDaz <shaunofshan@gmail.com>
2024-10-19 09:40:33 +0530

    website running

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,32 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.js"),
-      name: "MyComponentLibrary",
-      fileName: (format) => `my-component-library.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+  build:
+    mode === "library"
+      ? {
+          lib: {
+            entry: path.resolve(__dirname, "src/index.jsx"),
+            name: "MyComponentLibrary",
+            fileName: (format) => `my-component-library.${format}.js`,
+          },
+          rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+              globals: {
+                react: "React",
+                "react-dom": "ReactDOM",
+              },
+            },
+            plugins: [peerDepsExternal(), terser()],
+          },
+        }
+      : {
+          outDir: "dist", // For the website build
         },
-      },
-      plugins: [peerDepsExternal(), terser()],
-    },
-  },
-});
+}));
